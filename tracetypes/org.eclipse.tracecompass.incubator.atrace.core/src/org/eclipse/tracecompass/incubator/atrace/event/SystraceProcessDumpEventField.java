6a0b7a9883a32565a3426ff261bc7ee6a335b88f
Eva Terriault <eva.terriault@polymtl.ca>
2018-04-04 17:15:17 -0400

    WIP. Integration dans le parsing des traces atrace

--- tracetypes/org.eclipse.tracecompass.incubator.atrace.core/src/org/eclipse/tracecompass/incubator/atrace/event/SystraceProcessDumpEventField.java
+++ tracetypes/org.eclipse.tracecompass.incubator.atrace.core/src/org/eclipse/tracecompass/incubator/atrace/event/SystraceProcessDumpEventField.java
@@ -9,8 +9,10 @@
 
 package org.eclipse.tracecompass.incubator.atrace.event;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
@@ -44,7 +46,7 @@ public class SystraceProcessDumpEventField extends TmfEventField {
      * @param tid    the threadId
      * @param fields event fields (arguments)
      */
-    public SystraceProcessDumpEventField(String name, Integer cpu, Long ts, @Nullable Integer pid, @Nullable Integer tid, Map<String, Object> fields) {
+    public SystraceProcessDumpEventField(String name, Integer cpu, @Nullable Integer pid, @Nullable Integer tid, Map<String, Object> fields) {
         super(name, null, fields.entrySet().stream()
                 .map(entry -> new TmfEventField(entry.getKey(), entry.getValue(), null))
                 .toArray(ITmfEventField[]::new)); //TODO: What's value?
@@ -63,6 +65,29 @@ public class SystraceProcessDumpEventField extends TmfEventField {
     @Override
     public Object getValue() {
         return (Long) super.getValue();
+    }
+    public static @Nullable SystraceProcessDumpEventField parseLine(@Nullable String line) {
+        //Create new false event
+        if (line!=null)
+        {
+            String fName = "kthreadd";
+            int fCpu = 1;
+            //int fTid = rand.nextInt(100); //TODO: Not in our process dump
+            int fPid = 1;
+            int fPpid = 0;
+            //Long fStatus = rand.nextLong(); //TODO: Not sure what to put here
+
+            Map<@NonNull String, @NonNull Object> fields = new HashMap<>();
+            fields.put("name", fName); //$NON-NLS-1$
+            //fields.put("tid", (long)fTid); //$NON-NLS-1$
+            fields.put("pid", (long) fPid); //$NON-NLS-1$
+            fields.put("ppid", (long)fPpid); //$NON-NLS-1$
+            //fields.put("cpu", (long)fCpu); //$NON-NLS-1$
+            //fields.put("status", fStatus); //$NON-NLS-1$
+
+            return new SystraceProcessDumpEventField(fName, fCpu, fPid, fPpid, fields);
+        }
+        return null;
     }
 
 }
